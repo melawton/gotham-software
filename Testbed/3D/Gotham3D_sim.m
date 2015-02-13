c36d4ebbd88101ae5f9b6310d7327d4437bebc76
auszie <auszie@umich.edu>
2015-02-13 18:01:22 -0500

    Update all but 1 bug
    3D matrix bug adds infinite loop to the code

--- Testbed/3D/Gotham3D_sim.m
+++ Testbed/3D/Gotham3D_sim.m
@@ -170,85 +170,99 @@ for coil = 1 : numcurrcoils
 
     for x = 1:100
         x1 = (x - xcord)./1000; %from point to center in x direction
-        for y = 1:100
+        for y = 1:1000
             y1 = (y - ycord)./1000;
             for z = 1:100
                 z1 = (z - zcord)./1000;
-                for theta = 0:360*numcoils
-                    if theta == 0
-                        vector = [(x1- coilradius) y1 z1]; %vector
-                        deltaz = heightinc.*theta;
-                        z2 = z1 + deltaz;
-                        vectodx = [0 0 deltaz];
-                        normvectordx = vectordx./norm(vectordx);
-                        normvector = vector./norm(vector); 
-                        R = sqrt(x1.^2 + y1.^2 + z1.^2)- coilradius;
-                        dlxdr = cross(normvectordx,normvector);
-                        B = (mu0.*(current.*dlxdr))./R.^3;                      
-                        magfieldmatrix(x,y,z) = +B;
-                    elseif (theta > 0) && (90 >= theta)
-                        deltay = coilradius.*sind(theta);
-                        deltax = coilradius.*cosd(theta);
-                        deltaz = heightinc.*theta;
-                        y2 = y1 + deltay;
-                        x2 = x1 + deltax;
-                        z2 = z1 + deltaz;
-                        R = sqrt(x2.^2 + y2.^2 + z2.^2);
-                        vectodx = [deltax deltay deltaz];
-                        normvectordx = vectordx./norm(vectordx);
-                        vector = [x2 y2 z2];
-                        normvector = vector./norm(vector);
-                         magfieldmatrix(x,y,z) = + B; 
-                    elseif (theta > 90) && (180 >= theta)
-                        deltay = coilradius.*(1-cosd(theta));
-                        deltax = cooilradius.*(1+sind(theta));
-                        deltaz = heightinc.*theta;
-                        x2 = x1 + deltax;
-                        y2 = y2 + deltay;
-                        z2 = z1 + deltaz;
-                        R = sqrt(x2.^2 + y2.^2 + z2.^2);
-                        vectordx = [deltax deltay deltaz];
-                        vector = [x2 y2 z2];
-                        normvector = vector./norm(vector);
-                    elseif (theta > 180) && (270 >= theta)
-                        deltay = coilradius.*(-cosd(theta));
-                        deltax = cooilradius.*(2-sind(theta));
-                        deltaz = heightinc.*theta;
-                        x2 = x1 + deltax;
-                        y2 = y2 + deltay;
-                        z2 = z1 + deltaz;
-                        R = sqrt(x2.^2 + y2.^2 + z2.^2);
-                        vectordx = [deltax deltay deltaz];
-                        normvectordx = vectordx./norm(vectordx);
-                        vector = [x2 y2 z2];
-                        normvector = vector./norm(vector);
-                        dlxdr = cross(normvectordx,normvector);
-                        B = (mu0.*(current.*dlxdr))./R.^3;                       
-                        magfieldmatrix(x,y,z) = + B;
-                    else
-                        deltay = coilradius.*(1-cosd(theta));
-                        deltax = cooilradius.*(1+sind(theta));
-                        deltaz = heightinc.*theta;
-                        x2 = x1 + deltax;
-                        y2 = y2 + deltay;
-                        z2 = z1 + deltaz;
-                        R = sqrt(x2.^2 + y2.^2 + z2.^2);
-                        vector = [x2 y2 z2];
-                        vectodx = [deltax deltay deltaz];
-                        normvectordx = vectordx./norm(vectordx);
-                        normvector = vector./norm(vector);
-                        dlxdr = cross(normvectordx,normvector);
-                        B = (mu0.*(current.*dlxdr))./R.^3;                       
-                        magfieldmatrix(x,y,z) = + B;
-                   
+               for num = 1:numcoils
+                    for theta = 0:360
+                        while theta == 0
+                            vector = [(x1 - coilradius) y1 z1]; %vector
+                            deltaz = heightinc.*theta.*num;
+                            z2 = z1 + deltaz;
+                            vectordx = [0 0 deltaz];
+                            normvectordx = vectordx./norm(vectordx);
+                            normvector = vector./norm(vector); 
+                            R = sqrt((x1-coilradius).^2 + y1.^2 + z1.^2);
+                            dlxdr = (cross(normvectordx,normvector));
+                            B = norm(mu0.*(current.*dlxdr)./R.^3);                      
+                            magfieldmatrix(x,y,z) = + B
+                       
+                        while (theta > 0) && (90 >= theta)
+                            deltay = coilradius.*sind(theta);
+                            deltax = coilradius.*cosd(theta);
+                            deltaz = heightinc.*theta.*num;
+                            y2 = y1 + deltay;
+                            x2 = x1 + deltax;
+                            z2 = z1 + deltaz;
+                            R = sqrt(x2.^2 + y2.^2 + z2.^2);
+                            vectordx = [deltax deltay deltaz];
+                            normvectordx = vectordx./norm(vectordx);
+                            vector = [x2 y2 z2];
+                            normvector = vector./norm(vector);
+                            dlxdr = (cross(normvectordx,normvector));
+                            B = norm((mu0.*(current.*dlxdr))./R.^3);                       
+                            magfieldmatrix(x,y,z) = + B
+                       
+                        while (theta > 90) && (180 >= theta)
+                            deltay = coilradius.*(1-cosd(theta));
+                            deltax = coilradius.*(1+sind(theta));
+                            deltaz = heightinc.*theta.*num;
+                            x2 = x1 + deltax;
+                            y2 = y2 + deltay;
+                            z2 = z1 + deltaz;
+                            R = sqrt(x2.^2 + y2.^2 + z2.^2);
+                            vectordx = [deltax deltay deltaz];
+                            vector = [x2 y2 z2];
+                            dlxdr = cross(normvectordx,normvector);
+                            B = norm(mu0.*(current.*dlxdr)./R.^3);                       
+                            magfieldmatrix(x,y,z) = + B
+                      
+                        while  (theta > 180) && (270 >= theta)
+                            deltay = coilradius.*(-cosd(theta));
+                            deltax = coilradius.*(2-sind(theta));
+                            deltaz = heightinc.*theta.*num;
+                            x2 = x1 + deltax;
+                            y2 = y2 + deltay;
+                            z2 = z1 + deltaz;
+                            R = sqrt(x2.^2 + y2.^2 + z2.^2);
+                            vectordx = [deltax deltay deltaz];
+                            normvectordx = vectordx./norm(vectordx);
+                            vector = [x2 y2 z2];
+                            normvector = vector./norm(vector);
+                            dlxdr = cross(normvectordx,normvector);
+                            B = norm(mu0.*(current.*dlxdr)./R.^3);                       
+                            magfieldmatrix(x,y,z) = + B
+                      
+                        while (theta > 270) && (360 >= theta)
+                            deltay = coilradius.*(1-cosd(theta));
+                            deltax = coilradius.*(1+sind(theta));
+                            deltaz = heightinc.*theta;
+                            x2 = x1 + deltax;
+                            y2 = y2 + deltay;
+                            z2 = z1 + deltaz;
+                            R = sqrt(x2.^2 + y2.^2 + z2.^2);
+                            vector = [x2 y2 z2];
+                            vectodx = [deltax deltay deltaz];
+                            normvectordx = vectordx./norm(vectordx);
+                            normvector = vector./norm(vector);
+                            dlxdr = cross(normvectordx,normvector);
+                            B = mu0.*(current.*dlxdr)./R.^3;                       
+                            magfieldmatrix(x,y,z) = + norm(B)
+                        end
+                        end
+                        end
+                        end
+                        end
                     end
-                end
+               end
             end
         end
     end
-    
     counter = counter + 1;
-end
+end 
+   
+
 
 %{
 %Constants
