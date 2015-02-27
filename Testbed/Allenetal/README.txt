d819415ea76a2d1bcb508efa2e3497ad2e1fd097
smchn <samchen@umich.edu>
2015-02-26 19:33:50 -0500

    New Allenetal Folder
    There is a new folder called Allenetal, which is where we will be
    putting all our magnetic field filtering code.
    
    Mag_noise_filter.m is the actual function, mag_A and mag_B are both
    dummy lists to be used for testing.

---
+++ Testbed/Allenetal/README.txt
@@ -0,0 +1,4 @@
+Here is our code for the magnetic field filtering algorithm. 
+
+mag_A and mag_B are dummy lists of magnetometer readings, to be
+used for testing. 
---
+++ Testbed/Allenetal/mag_noise_filter.m
@@ -0,0 +1,10 @@
+function [] = mag_noise_filter()
+%Here is the code for the electromagnetic noise filtering algorithm
+
+
+
+
+disp('Hello, world!') %this line is just a test
+
+end
+
